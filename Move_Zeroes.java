6299c16bdddc386dd6b78050c0f2c01013b79d3f
Ani04p <113340267+Ani04p@users.noreply.github.com>
2023-06-05 14:18:25 +0530

    Create Move_Zeroes.java

---
+++ Move_Zeroes.java
@@ -0,0 +1,27 @@
+public class test1 {
+	
+	
+	 static void zerosToEnd(int A[], int n)
+	    {
+	        int count = 0;  // Count of non-zero elements
+	 
+	        
+	        for (int i = 0; i < n; i++)
+	            if (A[i] != 0)
+	                A[count++] = A[i]; 
+	        
+	        while (count < n)
+	            A[count++] = 0;
+	    }
+	 
+	  
+	    public static void main (String[] args)
+	    {
+	        int array[] = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
+	        int n = array.length;
+	        zerosToEnd(array, n);
+	        System.out.println("Array after pushing zeros to the back: ");
+	        for (int i=0; i<n; i++)
+	            System.out.print(array[i]+" ");
+	    }
+	}  
